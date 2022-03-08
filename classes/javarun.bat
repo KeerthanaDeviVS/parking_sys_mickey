6bb36d1ecc78a6d12059c613310cc863bb68c429
KeerthanaDeviVS <96223337+KeerthanaDeviVS@users.noreply.github.com>
2022-03-08 14:12:45 +0530

    Delete javarun.bat

--- classes/javarun.bat
+++
@@ -1,6 +0,0 @@
-
-@ECHO OFF
-javac -cp C:\Users\devi-pt4391\Downloads\MickeyLite\MickeyLite\lib\* *.java
-ECHO Success
-jar -cfm C:\Users\devi-pt4391\Downloads\MickeyLite\MickeyLite\lib\parkingsys.jar manifest.txt *.class
-PAUSE
